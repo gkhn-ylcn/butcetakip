2c7996f3671edc6e4e1a47838a269c7234d61f87
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-21 10:56:30 +0300

    Update cache name from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v1';
+const CACHE_NAME = 'butce-takip-v2';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
