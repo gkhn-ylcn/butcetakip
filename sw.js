331822363985f500591cea023d6da85343baf027
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-21 13:14:47 +0300

    Update cache name from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v4';
+const CACHE_NAME = 'butce-takip-v5';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
