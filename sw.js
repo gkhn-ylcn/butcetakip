6bf87f9eb3a6fe875ef89635a673e0010685877d
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-23 00:50:19 +0300

    Update CACHE_NAME to version v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v10';
+const CACHE_NAME = 'butce-takip-v11';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
