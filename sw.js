88d0af8ba7a303c65ab7cc42168143bf2e9cf5ba
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-23 01:02:19 +0300

    Update CACHE_NAME to version v12
    kredi kartı ödeme işlevi eklendi.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v11';
+const CACHE_NAME = 'butce-takip-v12';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
