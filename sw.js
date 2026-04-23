b6267629740d43e7a39b25996fefa71fb38ab9b4
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-23 15:38:08 +0300

    Update CACHE_NAME to butce-takip-v14
    parantez hatası giderildi.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v13';
+const CACHE_NAME = 'butce-takip-v14';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
