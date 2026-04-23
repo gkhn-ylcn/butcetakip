8eea48164c55f163a90bb92948fc28cb76ac574c
gkhn-ylcn <gkhnylcn@gmail.com>
2026-04-23 15:46:26 +0300

    Update CACHE_NAME to version v15
    veri temizleme açıklaması düzenlendi.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'butce-takip-v14';
+const CACHE_NAME = 'butce-takip-v15';
 // Önbelleğe alınacak dosyalar (Uygulamanın çalışması için şart olanlar)
 const ASSETS_TO_CACHE = [
   './',
